d0dfbf39275998fa5f1b4e7ef079fc09757e7b4f
F.Rabby <88621488+frabby338@users.noreply.github.com>
2022-04-12 04:07:51 +0600

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror bot', 'https://github.com/anime-republic/MIRROR-HUNTER')
+telegraph=TelegraphHelper('Mirror bot', 'https://github.com/frabby338/MIRROR-HUNTER')
